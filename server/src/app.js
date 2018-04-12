3624ad5e4625251365113d38b56cb2e75142ca06
Ali Kingravi <ali.kingravi@itccompliance.co.uk>
2018-04-12 16:11:57 +0100

    server setup complete

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World!'
+  })
+})
+
+// Listen to server
+app.listen(process.env.PORT || 8080)
