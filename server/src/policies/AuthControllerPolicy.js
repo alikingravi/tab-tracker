974016bef4a072020683cf75acaeb3943e7986b3
Ali Kingravi <ali.kingravi@itccompliance.co.uk>
2018-04-13 17:01:24 +0100

    testing db transactions in progress

--- server/src/policies/AuthControllerPolicy.js
+++ server/src/policies/AuthControllerPolicy.js
@@ -4,9 +4,7 @@ module.exports = {
   register (req, res, next) {
     const schema = {
       email: Joi.string().email(),
-      password: Joi.string().regex(
-        new RegExp('^[a-zA-Z0-9]{8, 32}$')
-      )
+      password: Joi.string()
     }
 
     const {error, value} = Joi.validate(req.body, schema)
@@ -23,9 +21,14 @@ module.exports = {
             error: `The password does not match the following rules:
               <br>
               1. It must contain ONLY the following characters: lower case, upper case, numerics.
-              <br>
+              <br:
               2. It must be at least 8 characters in length and not greater than 32 characters.
             `
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid Details'
           })
       }
     } else {
